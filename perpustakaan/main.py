e5d388b853e79545319b2f86df259154061e6141
cinopp <cindynovitri27@gmail.com>
2024-11-16 13:25:16 +0700

    Update main.py
    Aku memasukkan class menu

--- perpustakaan/main.py
+++ perpustakaan/main.py
@@ -46,7 +46,7 @@ class Buku:
         print("\n[Data Buku Telah Terhapus]")
         input("\nTekan [ENTER] untuk kembali ke menu.")
 
-    class Peminjam:
+class Peminjam:
     def _init_(self):
         self.daftarpeminjam = []
 
@@ -74,8 +74,59 @@ class Buku:
         self.daftarpeminjam = [peminjam for peminjam in self.daftarpeminjam if peminjam.lower() != nama.lower()]
         print("\n[Data Peminjam Telah Terhapus]")
         input("\nTekan [ENTER] untuk kembali ke menu.")
-    
 
+class Menu:
+    def _init_(self):
+        self.buku = Buku()
+        self.peminjam = Peminjam()
+
+    def tampilkan_menu(self):
+        while True:
+            os.system("cls")
+            print(" " * 12, "PERPUSTAKAAN MI", " " * 12)
+            print("Pilih daftar menu untuk mengakses Aplikasi :")
+            print("=" * 50)
+            print("[1] Lihat Daftar Buku")
+            print("[2] Cari Buku")
+            print("[3] Tambah Data Buku")
+            print("[4] Hapus Data Buku")
+            print("[5] Lihat Daftar Peminjam Buku")
+            print("[6] Tambah Peminjam Buku")
+            print("[7] Hapus Data Peminjam Buku")
+            print("[8] Keluar dari Aplikasi")
+
+            try:
+                kode = int(input("\nMasukkan kode menu yang ingin diakses : "))
+                self.pilih_menu(kode)
+            except ValueError:
+                print("Input harus berupa angka.")
+                time.sleep(1)
+
+    def pilih_menu(self, kode):
+        if kode == 1:
+            self.buku.lihat_daftarbuku()
+        elif kode == 2:
+            self.buku.cari_buku()
+        elif kode == 3:
+            self.buku.tambah_buku()
+        elif kode == 4:
+            self.buku.hapus_buku()
+        elif kode == 5:
+            self.peminjam.lihat_daftarpeminjam()
+        elif kode == 6:
+            self.peminjam.tambah_peminjam()
+        elif kode == 7:
+            self.peminjam.hapus_peminjam()
+        elif kode == 8:
+            print("Keluar dari Aplikasi...")
+            time.sleep(1)
+            exit()
+        else:
+            print("Kode yang Anda masukkan tidak valid.")
+            input("\nTekan [ENTER] untuk kembali ke menu.")
+
+
+    
 
 if __name__ == "__main__":
     perpustakaan = Perpustakaan()
