c781c458b4c6e466a7cc9b979d481dc764b7e502
nashwataled <talitha.nashwa2006@gmail.com>
2024-11-16 13:20:14 +0700

    Update main.py
    kami jadi ubah class nya dan kami bagi kerja ulang

--- perpustakaan/main.py
+++ perpustakaan/main.py
@@ -1,52 +1,9 @@
 import os
 import time
 
-class Perpustakaan:
+class Buku:
     def __init__(self):
-        self.daftarbuku = []  # Format: [(judul, penulis, tahun)]
-        self.daftarpeminjam = []  # Format: [(nama, judul, tanggal)]
-
-    def tampilkan_menu(self):
-        os.system("cls")
-        print(" " * 12, "PERPUSTAKAAN MI", " " * 12)
-        print("Pilih daftar menu untuk mengakses Aplikasi :")
-        print("=" * 50)
-        print("[1] Lihat Daftar Buku")
-        print("[2] Cari Buku")
-        print("[3] Tambah Data Buku")
-        print("[4] Hapus Data Buku")
-        print("[5] Lihat Daftar Peminjam Buku")
-        print("[6] Tambah Peminjam Buku")
-        print("[7] Hapus Data Peminjam Buku")
-        print("[8] Keluar dari Aplikasi")
-
-
-        kode = int(input("\nMasukkan kode menu yang ingin diakses : "))
-        self.pilih_menu(kode)
-
-    def pilih_menu(self, kode):
-        if kode == 1:
-            self.lihat_daftarbuku()
-        elif kode == 2:
-            self.cari_buku()
-        elif kode == 3:
-            self.tambah_buku()
-        elif kode == 4:
-            self.hapus_buku()
-        elif kode == 5:
-            self.lihat_daftarpeminjam()
-        elif kode == 6:
-            self.tambah_peminjam()
-        elif kode == 7:
-            self.hapus_peminjam()
-        elif kode == 8:
-            print("Keluar dari Aplikasi...")
-            time.sleep(1)
-            exit()
-        else:
-            print("Kode yang Anda masukkan tidak valid.")
-            input("\nTekan [ENTER] untuk kembali ke menu.")
-            self.tampilkan_menu()
+        self.daftarbuku = []
 
     def lihat_daftarbuku(self):
         os.system("cls")
@@ -58,7 +15,6 @@ class Perpustakaan:
             for i, (judul, penulis, tahun) in enumerate(self.daftarbuku, start=1):
                 print(f"{i}. {judul} | {penulis} | {tahun}")
         input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
 
     def cari_buku(self):
         os.system("cls")
@@ -72,7 +28,6 @@ class Perpustakaan:
         if not found:
             print("Hasil pencarian tidak ditemukan.")
         input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
 
     def tambah_buku(self):
         os.system("cls")
@@ -83,7 +38,6 @@ class Perpustakaan:
         self.daftarbuku.append((judul, penulis, tahun))
         print("[Data Buku Berhasil Ditambahkan]")
         input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
 
     def hapus_buku(self):
         os.system("cls")
@@ -91,39 +45,6 @@ class Perpustakaan:
         self.daftarbuku = [data for data in self.daftarbuku if data[0].lower() != judul.lower()]
         print("\n[Data Buku Telah Terhapus]")
         input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
-
-   def lihat_daftarpeminjam(self):
-        os.system("cls")
-        print("DAFTAR PEMINJAM BUKU")
-        print("=" * 50)
-        if not self.daftarpeminjam:
-            print("\n[Data tidak tersedia]")
-        else:
-            for i, (nama, judul, tanggal) in enumerate(self.daftarpeminjam, start=1):
-                print(f"{i}. {nama} | {judul} | {tanggal}")
-        input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
-
-    def tambah_peminjam(self):
-        os.system("cls")
-        print("TAMBAH PEMINJAM BUKU")
-        nama = input("Nama Peminjam : ")
-        judul = input("Judul Buku : ")
-        tanggal = input("Tanggal Peminjaman : ")
-        self.daftarpeminjam.append((nama, judul, tanggal))
-        print("[Data Peminjam Berhasil Ditambahkan]")
-        input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
-
-    def hapus_peminjam(self):
-        os.system("cls")
-        nama = input("Masukkan Nama Peminjam yang Ingin Dihapus : ")
-        self.daftarpeminjam = [data for data in self.daftarpeminjam if data[0].lower() != nama.lower()]
-        print("\n[Data Peminjam Telah Terhapus]")
-        input("\nTekan [ENTER] untuk kembali ke menu.")
-        self.tampilkan_menu()
-
     
 
 
