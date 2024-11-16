051f508ea70f6fd9ee102dfb0f5433367165fd74
cinopp <cindynovitri27@gmail.com>
2024-11-16 11:30:47 +0700

    Update main.py
    saya menambahkan bagian cari, tambah, dan hapus buku

--- perpustakaan/main.py
+++ perpustakaan/main.py
@@ -59,7 +59,38 @@ class Perpustakaan:
         input("\nTekan [ENTER] untuk kembali ke menu.")
         self.tampilkan_menu()
 
-   
+    def cari_buku(self):
+        os.system("cls")
+        judul = input("\nMasukan judul buku yang ingin dicari: ")
+        found = False
+        for data in self.daftarbuku:
+            if data[0].lower() == judul.lower():
+                print(f"Buku ditemukan: {data[0]} | {data[1]} | {data[2]}")
+                found = True
+                break
+        if not found:
+            print("Hasil pencarian tidak ditemukan.")
+        input("\nTekan [ENTER] untuk kembali ke menu.")
+        self.tampilkan_menu()
+
+    def tambah_buku(self):
+        os.system("cls")
+        print("TAMBAH BUKU")
+        judul = input("Judul Buku : ")
+        penulis = input("Penulis Buku : ")
+        tahun = input("Tahun Terbit : ")
+        self.daftarbuku.append((judul, penulis, tahun))
+        print("[Data Buku Berhasil Ditambahkan]")
+        input("\nTekan [ENTER] untuk kembali ke menu.")
+        self.tampilkan_menu()
+
+    def hapus_buku(self):
+        os.system("cls")
+        judul = input("Masukkan judul buku yang ingin dihapus : ")
+        self.daftarbuku = [data for data in self.daftarbuku if data[0].lower() != judul.lower()]
+        print("\n[Data Buku Telah Terhapus]")
+        input("\nTekan [ENTER] untuk kembali ke menu.")
+        self.tampilkan_menu()
 
     
 
